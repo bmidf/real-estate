060ed95c10a6f933a8db136b755b25e2f27f579b
bmidf <55makharadze@gmail.com>
2024-09-11 18:47:06 +0400

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{marginLeft: '8%', marginRight: '8%', textAlign: 'left', padding: '20px', color: 'red'}}>
+      <div>
+          <h1>REDBERRY</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
