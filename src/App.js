3ba2a9c4c11559e5f306fccb5854999f1f9301c4
bmidf <55makharadze@gmail.com>
2024-09-19 19:21:30 +0400

    Fixes for Github Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Listing from './pages/Listing';
@@ -9,14 +9,12 @@ import { Container } from 'react-bootstrap';
 function App() {
   return (
     <Container className="App">
-      <Router>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/real-estate/:id" element={<Listing />} />
           <Route path="/add-listing" element={<AddListing />} />
         </Routes>
-      </Router>
     </Container>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
 
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
